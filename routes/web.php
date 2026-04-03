9ea260102aeba7b365610cea321f84461ef7e0d3
katherinek727 <katherinek727@outlook.com>
2026-04-03 03:08:36 +0300

    feat(routes): register task resource routes with root redirect

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', static fn() => redirect()->route('tasks.index'));
+
+Route::resource('tasks', TaskController::class);
